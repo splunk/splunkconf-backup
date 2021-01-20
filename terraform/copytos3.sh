3365cce6e6db962e38873890bfb54ee39bdf756e
Matthieu Araman <maraman@splunk.com>
2021-01-20 08:31:14 +0100

    path updates

--- terraform/copytos3.sh
+++ terraform/copytos3.sh
@@ -2,6 +2,8 @@
 
 # Matthieu Araman , Splunk
 # this script called by terraform
+# 20210111 change paths to ref new buckets dirstructure
+# 20210113 path change
 
 
 # first arg = bucket install
@@ -9,6 +11,6 @@
 
 s3_install=$1
 s3_backup=$2
-aws s3 sync ./packaged s3://${s3_install}/packaged/ --storage-class STANDARD_IA; 
-aws s3 sync ./splunkconf-backup s3://${s3_backup}/splunkconf-backup/ --storage-class STANDARD_IA; 
-aws s3 sync ./install s3://${s3_install}/install/ --storage-class STANDARD_IA
+aws s3 sync ../buckets/bucket-backup/splunkconf-backup s3://${s3_backup}/splunkconf-backup/ --storage-class STANDARD_IA; 
+aws s3 sync ../buckets/bucket-install/packaged s3://${s3_install}/packaged/ --storage-class STANDARD_IA; 
+aws s3 sync ../buckets/bucket-install/install s3://${s3_install}/install/ --storage-class STANDARD_IA

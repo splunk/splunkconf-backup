9942b77bad57f2d2fa542cd962ee2ce7f764071a
Matthieu Araman <maraman@splunk.com>
2021-06-02 23:29:36 +0200

    add safeguard to not report errors when dir empty so terraform happy even when doing nothing

--- terraform/build-idx-scripts.sh
+++ terraform/build-idx-scripts.sh
@@ -9,10 +9,12 @@ fi
 
 instancetype=$1
 
-
+mkdir -p ./build-idx
 find ./build-idx -type f -exec rm {} \;
 mkdir -p ./build-idx/usr/local/bin/ ./build-idx/opt/splunk/scripts
-cp -p ./scripts-template/splunkconf-aws-terminate-idx.sh ./build-idx/usr/local/bin/
-chmod 555 ./build-idx/usr/local/bin/splunkconf-aws-terminate-idx.sh
+if [ -e "./scripts-template/splunkconf-aws-terminate-idx.sh" ]; then
+  cp -p ./scripts-template/splunkconf-aws-terminate-idx.sh ./build-idx/usr/local/bin/
+  chmod 555 ./build-idx/usr/local/bin/splunkconf-aws-terminate-idx.sh
+fi
 mkdir -p ../buckets/bucket-backup/splunkconf-backup/${instancetype}
 tar -C"./build-idx" --exclude "._*" --exclude ".DS*" -zcvf ../buckets/bucket-backup/splunkconf-backup/${instancetype}/backupconfsplunk-scripts-initial.tar.gz ./opt/splunk/scripts ./usr/local/bin
--- terraform/build-nonidx-scripts.sh
+++ terraform/build-nonidx-scripts.sh
@@ -9,10 +9,12 @@ fi
 
 instancetype=$1
 
-
+mkdir -p ./build-nonidx
 find ./build-nonidx -type f -exec rm {} \;
 mkdir -p ./build-nonidx/usr/local/bin/ ./build-nonidx/opt/splunk/scripts
-cp -p ./scripts-template/aws-update-dns.sh ./build-nonidx/usr/local/bin/aws-postinstall.sh
-chmod 555 ./build-idx/usr/local/bin/aws-postinstall.sh
+if [ -e "./scripts-template/aws-update-dns.sh" ]; then
+  cp -p ./scripts-template/aws-update-dns.sh ./build-nonidx/usr/local/bin/aws-postinstall.sh
+  chmod 555 ./build-idx/usr/local/bin/aws-postinstall.sh
+fi
 mkdir -p ../buckets/bucket-backup/splunkconf-backup/${instancetype}
 tar -C"./build-idx" --exclude "._*" --exclude ".DS*" -zcvf ../buckets/bucket-backup/splunkconf-backup/${instancetype}/backupconfsplunk-scripts-initial.tar.gz ./opt/splunk/scripts ./usr/local/bin

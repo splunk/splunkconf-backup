18a9938d97bc0bda1c90e0d1d0890a73aaf06fc0
Matthieu Araman <maraman@splunk.com>
2022-03-06 18:51:48 +0100

    imdsv2 var support here

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -105,10 +105,9 @@ resource aws_launch_template splunk-bastion {
   }
   metadata_options {
     http_endpoint               = "enabled"
-    http_tokens                 = "required"
+    http_tokens                 = (var.imdsv2 == "required" ? "required" : "optional")
     http_put_response_hop_limit = 1
   }
   user_data = filebase64("../buckets/bucket-install/install/user-data-bastion.txt")
 }
-
 

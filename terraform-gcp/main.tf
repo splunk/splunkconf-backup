de900a0150707f7367286a403aadd5f569c08780
Matthieu Araman <maraman@splunk.com>
2022-06-16 21:14:13 +0200

    remove network module version constraint

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -2,7 +2,7 @@
 
 module "network" {
   source       = "terraform-google-modules/network/google"
-  version      = "2.5.0"
+  #version      = "2.5.0"
   network_name = "my-vpc-network"
   project_id   = var.project
 
@@ -24,7 +24,7 @@ module "network" {
 
 module "network_routes" {
   source       = "terraform-google-modules/network/google//modules/routes"
-  version      = "2.5.0"
+  #version      = "2.5.0"
   network_name = module.network.network_name
   project_id   = var.project
 
@@ -44,7 +44,7 @@ module "network_routes" {
 
 module "network_fabric-net-firewall" {
   source                  = "terraform-google-modules/network/google//modules/fabric-net-firewall"
-  version                 = "2.5.0"
+  #version                 = "2.5.0"
   project_id              = var.project
   network                 = module.network.network_name
   internal_ranges_enabled = true

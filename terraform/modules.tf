d834678c305b99c3032790e0e0402cc86eda5850
Matthieu Araman <maraman@splunk.com>
2022-08-10 09:16:40 +0200

    move variables to a module

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,3 +1,4 @@
-#module "ssh" {
-#  source = "./modules/ssh"
-#}
+# variables moves to a module so other modules can easily import , including when launched independently
+module "variables" {
+  source = "./modules/variables"
+}
---
+++ terraform/modules/ssh/modules.tf
@@ -0,0 +1,3 @@
+module "variables" {
+  source = "../variables"
+}

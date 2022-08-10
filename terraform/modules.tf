ea55b92c6dc70959925e907b4f8751f97f6e2985
Matthieu Araman <maraman@splunk.com>
2022-08-10 13:51:35 +0200

    disable module for variables

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,7 +1,8 @@
 # variables moves to a module so other modules can easily import , including when launched independently
-module "variables" {
-  source = "./modules/variables"
-}
+# commented as require more abstraction to work
+#module "variables" {
+#  source = "./modules/variables"
+#}
 
 # include module directly
 module "network" {

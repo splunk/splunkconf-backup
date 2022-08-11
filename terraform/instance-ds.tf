b5801467b658e0f6f764803ee55efe248a0456ce
Matthieu Araman <maraman@splunk.com>
2022-08-11 13:29:14 +0200

    fix ssk key name with remote state

--- terraform/instance-ds.tf
+++ terraform/instance-ds.tf
@@ -239,7 +239,7 @@ resource "aws_launch_template" "splunk-ds" {
   #name          = "splunk-ds"
   name_prefix   = "splunk-ds-"
   image_id      = data.aws_ssm_parameter.linuxAmi.value
-  key_name      = aws_key_pair.master-key.key_name
+  key_name      = data.terraform_remote_state.ssh.outputs.ssh_key_name
   instance_type = "t3a.nano"
   block_device_mappings {
     device_name = "/dev/xvda"

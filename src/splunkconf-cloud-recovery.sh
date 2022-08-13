2832391d9f3082280d35ef77ef061f900fa7c672
Matthieu Araman <maraman@splunk.com>
2022-08-13 14:47:18 +0200

    typo fix

--- src/splunkconf-cloud-recovery.sh
+++ src/splunkconf-cloud-recovery.sh
@@ -471,9 +471,9 @@ tag_replacement () {
       echo "using splunkdnszone ${splunkdnszone} from instance tags [license] master_uri=${splunktargetlm}.${splunkdnszone}:8089 (lm name or a cname alias to it)  " >> /var/log/splunkconf-cloud-recovery-info.log
       ${SPLUNK_HOME}/bin/splunk btool server list license --debug | grep -v m/d | grep master_uri | cut -d" " -f 1 | head -1 |  xargs -L 1 sed -i -e "s%^[^#]{1}.*master_uri.*=.*$%master_uri=https://${splunktargetlm}.${splunkdnszone}:8089%" 
       echo "trying also lm replacement for DS"  >> /var/log/splunkconf-cloud-recovery-info.log
-      FILM="${SPLUNK_HOME}/etc/deployment-apps/${splunkorg}_full_licence_slave/local/server.conf"
+      FILM="${SPLUNK_HOME}/etc/deployment-apps/${splunkorg}_full_license_slave/local/server.conf"
       if [ -e "$FILM" ]; then
-        find ${SPLUNK_HOME}/etc/deployment-apps/org_full_licence_slave/local/server.conf -print  |  xargs -L 1 sed -i -e "s%^[^#]{1}.*master_uri.*=.*$%master_uri=https://${splunktargetlm}.${splunkdnszone}:8089%"
+        find ${FILM} -print  |  xargs -L 1 sed -i -e "s%^[^#]{1}.*master_uri.*=.*$%master_uri=https://${splunktargetlm}.${splunkdnszone}:8089%"
       else 
         echo "$FILM doesnt exist, not trying to replace master_uri for LM in deployment-apps"
       fi

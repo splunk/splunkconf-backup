4bbbf1a66e57543be4e279d75d223f88e376dd38
Matthieu Araman <maraman@splunk.com>
2022-08-16 19:38:50 +0200

    move instance var to template and asg

--- terraform/instance-extra/instance-idx.tf
+++ terraform/instance-extra/instance-idx.tf
@@ -312,7 +312,7 @@ resource "aws_launch_template" "splunk-idx" {
   name          = "splunk-idx"
   image_id      = data.aws_ssm_parameter.linuxAmi.value
   key_name      = data.terraform_remote_state.ssh.outputs.ssh_key_name
-  instance_type = "t3a.nano"
+  instance_type = local.instance-type-indexer
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {

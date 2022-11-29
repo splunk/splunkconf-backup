b9076b4a784c140758e79d54083b2faaef354173
Matthieu Araman <maraman@splunk.com>
2022-11-29 09:51:39 +0100

    add specific outbound group for LB

--- terraform/sg-splunk-outbound.tf
+++ terraform/sg-splunk-outbound.tf
@@ -19,3 +19,38 @@ resource "aws_security_group_rule" "idx_outbound_all" {
   description       = "allow all outbound traffic"
 }
 
+####
+
+resource "aws_security_group" "splunk-lb-outbound" {
+  name_prefix        = "splunk-lb-outbound"
+  description = "Outbound Security groupi for ELB"
+  vpc_id      = local.master_vpc_id
+  tags = {
+    Name = "splunk"
+  }
+}
+
+resource "aws_security_group_rule" "lb_outbound_hecrest" {
+  security_group_id = aws_security_group.splunk-lb-outbound.id
+  type              = "egress"
+  from_port         = 8088
+  to_port           = 8089
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "allow outbound traffic for hec and rest api ports"
+}
+
+resource "aws_security_group_rule" "lb_outbound_webui" {
+  security_group_id = aws_security_group.splunk-lb-outbound.id
+  type              = "egress"
+  from_port         = 8000
+  to_port           = 8000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "allow outbound traffic for webui port"
+}
+
+
+
+
+

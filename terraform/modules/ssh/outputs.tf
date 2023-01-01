e47b8bbaa178b2e498a93cd028ff243ec5944579
Matthieu Araman <maraman@splunk.com>
2023-01-01 08:47:27 +0100

    change ssh generation to terraform and store in secret manager for reuse

--- terraform/modules/ssh/outputs.tf
+++ terraform/modules/ssh/outputs.tf
@@ -2,5 +2,15 @@
 
 output "ssh_key_name" {
   description = "ssh key name"
-  value       = aws_key_pair.master-key.key_name
+  value       = aws_key_pair.mykey.key_name
+}
+
+output "secretsmanager_secretssh" {
+  description = "secret manager id for ssh priv key"
+  value = aws_secretsmanager_secret.mykey.id
+}
+
+output "secretsmanager_secretssh_version" {
+  description = "secret manager version id for ssh priv key"
+  value = aws_secretsmanager_secret_version.mykey.id
 }
--- terraform/modules/ssh/ssh.tf
+++ terraform/modules/ssh/ssh.tf
@@ -5,12 +5,31 @@
 #Please note that this code expects SSH key pair to exist in default dir under 
 #users home directory, otherwise it will fail
 
+# use this version if you want to import existing key generated outside terraform
+
 #Create key-pair for logging into EC2 
-resource "aws_key_pair" "master-key" {
-  provider   = aws.region-primary
-  key_name_prefix   = "mykey"
-  public_key = file("~/.ssh/id_rsa.pub")
+#resource "aws_key_pair" "mykey" {
+#  provider   = aws.region-primary
+#  key_name_prefix   = "mykey"
+#  public_key = file("~/.ssh/id_rsa.pub")
+#}
+
+resource "tls_private_key" "mykey" {
+  algorithm = "ED25519"
 }
 
+resource "aws_key_pair" "mykey" {
+  provider   = aws.region-primary
+  key_name_prefix   = "mykey"
+  public_key = tls_private_key.mykey.public_key_openssh
+}
 
+resource "aws_secretsmanager_secret" "mykey" {
+  name = "mykey"
+}
+
+resource "aws_secretsmanager_secret_version" "mykey" {
+  secret_id     = aws_secretsmanager_secret.mykey.id
+  secret_string = tls_private_key.mykey.private_key_pem
+}
 

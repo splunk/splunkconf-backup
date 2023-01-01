bc737f135c3638a5fc8f9b246310ff42688d6529
Matthieu Araman <maraman@splunk.com>
2023-01-01 12:27:44 +0100

    add inputs for network module

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -3,7 +3,17 @@
 
 module "network" {
   source = "./modules/network"
+  vpc_cidr_block=var.vpc_cidr_block
+  cidr_subnet_pub_1=var.cidr_subnet_pub_1
+  cidr_subnet_pub_2=var.cidr_subnet_pub_2
+  cidr_subnet_pub_3=var.cidr_subnet_pub_3
+  cidr_subnet_priv_1=var.cidr_subnet_priv_1
+  cidr_subnet_priv_2=var.cidr_subnet_priv_2
+  cidr_subnet_priv_3=var.cidr_subnet_priv_3
+  use_nat_gateway=var.use_nat_gateway
+  nat_gateway_ha=var.nat_gateway_ha
 }
+
 
 module "ssh" {
   source = "./modules/ssh"
--- terraform/modules/network/providers.tf
+++
@@ -1,7 +0,0 @@
-
-provider "aws" {
-#  profile = var.profile
-  region  = var.region-primary
-  alias   = "region-primary"
-}
-

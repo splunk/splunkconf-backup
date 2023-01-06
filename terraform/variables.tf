d126d7930e70289c7385b86ff1968fc84cb5b52c
Matthieu Araman <maraman@splunk.com>
2023-01-06 14:42:08 +0100

    fix variable condition with bool

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -317,8 +317,8 @@ locals {
   instance-type-mc      = (local.env == "min" ? var.instance-type-mc-min : var.instance-type-mc-default)
   instance-type-ds      = (local.env == "min" ? var.instance-type-ds-min : var.instance-type-ds-default)
   instance-type-sh      = (local.env == "min" ? var.instance-type-sh-min : var.instance-type-sh-default)
-  ds-nb                 = (var.ds-enable == "true" ? 1 : 0)
-  mc-nb                 = (var.mc-enable == "true" ? 1 : 0)
+  ds-nb                 = (var.ds-enable ? 1 : 0)
+  mc-nb                 = (var.mc-enable ? 1 : 0)
 }
 
 variable "associate_public_ip" {

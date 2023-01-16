199988e277af43a0f5c4aff80ee1a095cbb4d0f8
Matthieu Araman <maraman@splunk.com>
2023-01-16 09:39:50 +0100

    change default regions to the us ones

--- terraform/variables-global.tf
+++ terraform/variables-global.tf
@@ -5,22 +5,23 @@ variable "profile" {
 }
 
 # do not use, replaced by region-primary
+# we cant remove easily du to the way TF store things in state
 variable "region-master" {
   description = "(legacy, please use region-primary) ) primary AWS region to use (us-east-1,eu-west-3,...)"
   type        = string
-  default     = "eu-west-3"
+  default     = "us-east-1"
 }
 
 variable "region-primary" {
   description = "primary AWS region to use (us-east-1,eu-west-3,...)"
   type        = string
-  default     = "eu-west-3"
+  default     = "us-east-1"
 }
 
 variable "region-secondary" {
-  description = "secondary (backup) AWS region to use (us-east-1,eu-west-3,...)"
+  description = "secondary (backup) AWS region to use (us-west-1,eu-west-1,...)"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-west-1"
 }
 
 variable "splunktargetenv" {

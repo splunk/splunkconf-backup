ef8b2f0d47cbe0c4a666912d24e38d1268edc00e
Matthieu Araman <maraman@splunk.com>
2023-01-22 15:59:22 +0100

    typo fix

--- terraform/modules/network/route53.tf
+++ terraform/modules/network/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_zone" "dnszone" {
   name    = var.dns-zone-name
   comment = "public dns zone for splunk dns updates"
-  provisionner "local-exec" {
+  provisioner "local-exec" {
     count = var.enable-ns-glue-aws ? 1 : 0
     command = "sleep 10; ./scripts/route53-delegatetop.sh ${aws_route53_zone.dnszone.name} ${var.region} ${aws_route53_zone.dnszone.zone_id} ${var.ns_ttl}"
  

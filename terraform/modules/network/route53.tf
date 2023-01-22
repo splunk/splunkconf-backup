ef5faf58022a2df66799ae630a2c0c09d4d0bc6f
Matthieu Araman <maraman@splunk.com>
2023-01-22 16:04:13 +0100

    comment count as not supported here

--- terraform/modules/network/route53.tf
+++ terraform/modules/network/route53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_zone" "dnszone" {
   name    = var.dns-zone-name
   comment = "public dns zone for splunk dns updates"
   provisioner "local-exec" {
-    count = var.enable-ns-glue-aws ? 1 : 0
+    #count = var.enable-ns-glue-aws ? 1 : 0
     command = "sleep 10; ./scripts/route53-delegatetop.sh ${aws_route53_zone.dnszone.name} ${var.region} ${aws_route53_zone.dnszone.zone_id} ${var.ns_ttl}"
  
  } 

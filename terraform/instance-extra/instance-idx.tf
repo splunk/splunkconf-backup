30f0f4aa3acdda35b27331ffedfe0d30c267c40d
Matthieu Araman <maraman@splunk.com>
2023-04-27 21:48:57 +0200

    enable toggle for hec elb (more)

--- terraform/instance-extra/instance-idx.tf
+++ terraform/instance-extra/instance-idx.tf
@@ -530,7 +530,7 @@ resource "aws_lb" "idxhec-ack" {
 
 resource "aws_alb_listener" "idxhec-noack" {
   count    = var.use_elb ? 1 : 0
-  load_balancer_arn = aws_lb.idxhec-noack.arn[count.index]
+  load_balancer_arn = aws_lb.idxhec-noack.arn[0]
   port              = 8088
   # change here for HTTPS
   protocol = "HTTP"
@@ -542,7 +542,7 @@ resource "aws_alb_listener" "idxhec-noack" {
 
 resource "aws_alb_listener" "idxhec-ack" {
   count    = var.use_elb_ack ? 1 : 0
-  load_balancer_arn = aws_lb.idxhec-ack.arn[count.index]
+  load_balancer_arn = aws_lb.idxhec-ack.arn[0]
   port              = 8088
   # change here for HTTPS
   protocol = "HTTP"

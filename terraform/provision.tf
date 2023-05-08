40133d36ebc549b1c166ba45581f3f1178b21882
Matthieu Araman <maraman@splunk.com>
2023-05-08 14:00:45 +0200

    rename splunk_smartstore_site_number to splunksmartstoresitenumber

--- terraform/provision.tf
+++ terraform/provision.tf
@@ -19,7 +19,7 @@ resource "local_file" "ansible_vars_tf" {
     license_manager_name: ${local.license_manager_name}
     deploymentserver_name: ${local.deploymentserver_name}
     smartstore_uri: ${local.smartstore_uri}
-    smartstore_site_number: ${var.smartstore_site_number}
+    smartstore_site_number: ${var.splunksmartstoresitenumber}
     dns_zone_name: ${var.dns-zone-name}
   tasks:
   - name: create directories for target jinja

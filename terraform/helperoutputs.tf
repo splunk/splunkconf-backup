880550a62b1f7679796011df1c1414fa37d26472
Matthieu Araman <maraman@splunk.com>
2023-05-10 19:38:03 +0200

    add helper output for getmycredentials

---
+++ terraform/helperoutputs.tf
@@ -0,0 +1,9 @@
+
+locals {
+  helper-getmycredentials = "./getmycredentials.sh ${var.region-primary} ${aws_secretsmanager_secret.splunk_admin.id} ${module.ssh.splunk_ssh_key_arn}"
+}
+
+output "helper-getmycredentials" {
+  value = local.helper-getmycredentials
+  description = "command to run to get credentials (if authorized)"
+}

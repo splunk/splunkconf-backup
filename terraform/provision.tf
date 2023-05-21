fc90a810444c2c2b34a9badd7561660189268aac
Matthieu Araman <maraman@splunk.com>
2023-05-21 15:30:37 +0200

    split ansible in 2 parts, central and local

--- terraform/provision.tf
+++ terraform/provision.tf
@@ -69,5 +69,5 @@ resource "null_resource" "bucket_sync_worker" {
   provisioner "local-exec" {
     command = "./scripts/copytos3-worker.sh ${aws_s3_bucket.s3_install.id} ${aws_s3_bucket.s3_backup.id}"
   }
-  depends_on = [null_resource.build-idx-scripts, null_resource.build-nonidx-scripts, aws_s3_bucket_lifecycle_configuration.s3_install_lifecycle, aws_s3_bucket_lifecycle_configuration.s3_backup_lifecycle, aws_s3_bucket_versioning.s3_install_versioning, aws_s3_bucket_versioning.s3_backup_versioning, local_file.ansible_vars_tf local_file.ansible_jinja_byhost_tf]
+  depends_on = [null_resource.build-idx-scripts, null_resource.build-nonidx-scripts, aws_s3_bucket_lifecycle_configuration.s3_install_lifecycle, aws_s3_bucket_lifecycle_configuration.s3_backup_lifecycle, aws_s3_bucket_versioning.s3_install_versioning, aws_s3_bucket_versioning.s3_backup_versioning, local_file.ansible_vars_tf, local_file.ansible_jinja_byhost_tf]
 }

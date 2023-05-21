d1cec8f63cc149120497ad6671d3136a023ad007
Matthieu Araman <maraman@splunk.com>
2023-05-21 22:51:55 +0200

    general form + subdir + sync

--- terraform/scripts/copytos3-worker.sh
+++ terraform/scripts/copytos3-worker.sh
@@ -6,6 +6,7 @@
 # 20210113 path change
 # 20230314 version just for ansible playbook for worker
 # 20230521 split ansible into 2 files
+# 20230521 general form + subdir + sync
 
 
 # first arg = bucket install
@@ -16,5 +17,6 @@ s3_backup=$2
 #aws s3 sync ../buckets/bucket-backup/splunkconf-backup s3://${s3_backup}/splunkconf-backup/ --storage-class STANDARD_IA; 
 #aws s3 sync ../buckets/bucket-install/packaged s3://${s3_install}/packaged/ --storage-class STANDARD_IA; 
 #aws s3 sync ../buckets/bucket-install/install s3://${s3_install}/install/ --storage-class STANDARD_IA
-aws s3 cp ./ansible_jinja_tf.yml s3://${s3_install}/install/ --storage-class STANDARD_IA
-aws s3 cp ./ansible_jinja_byhost_tf.yml s3://${s3_install}/install/ --storage-class STANDARD_IA
+aws s3 sync ./ansible* s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
+#aws s3 cp ./ansible_jinja_tf.yml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
+#aws s3 cp ./ansible_jinja_byhost_tf.yml s3://${s3_install}/install/ --storage-class STANDARD_IA

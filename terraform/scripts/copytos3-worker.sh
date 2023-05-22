6571083773c02f51ea269eb237f9d83fd3fe5871
Matthieu Araman <maraman@splunk.com>
2023-05-22 09:36:09 +0200

    sync->cp

--- terraform/scripts/copytos3-worker.sh
+++ terraform/scripts/copytos3-worker.sh
@@ -7,6 +7,7 @@
 # 20230314 version just for ansible playbook for worker
 # 20230521 split ansible into 2 files
 # 20230521 general form + subdir + sync
+# 20230522 fallback to cp version
 
 
 # first arg = bucket install
@@ -17,7 +18,7 @@ s3_backup=$2
 #aws s3 sync ../buckets/bucket-backup/splunkconf-backup s3://${s3_backup}/splunkconf-backup/ --storage-class STANDARD_IA; 
 #aws s3 sync ../buckets/bucket-install/packaged s3://${s3_install}/packaged/ --storage-class STANDARD_IA; 
 #aws s3 sync ../buckets/bucket-install/install s3://${s3_install}/install/ --storage-class STANDARD_IA
-aws s3 sync ./ansible* s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
+aws s3 cp ./ansible* s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
 aws s3 cp ../helpers/getmycredentials.sh s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
 aws s3 cp ./inventory.yaml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
 #aws s3 cp ./ansible_jinja_tf.yml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA

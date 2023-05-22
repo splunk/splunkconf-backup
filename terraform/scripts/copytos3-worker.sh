6442da91ae889f7226e4d58cb18f3eb4e396abc4
Matthieu Araman <maraman@splunk.com>
2023-05-22 10:34:46 +0200

    cp version

--- terraform/scripts/copytos3-worker.sh
+++ terraform/scripts/copytos3-worker.sh
@@ -22,4 +22,4 @@ s3_backup=$2
 aws s3 cp ../helpers/getmycredentials.sh s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
 aws s3 cp ./inventory.yaml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
 aws s3 cp ./ansible_jinja_tf.yml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
-aws s3 cp ./ansible_jinja_byhost_tf.yml s3://${s3_install}/install/ --storage-class STANDARD_IA
+aws s3 cp ./ansible_jinja_byhost_tf.yml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA

07d4698848a106180f29450dd22ad20978ff1c3d
Matthieu Araman <maraman@splunk.com>
2023-05-29 15:51:58 +0200

    add more file for worker

--- terraform/scripts/copytos3-worker.sh
+++ terraform/scripts/copytos3-worker.sh
@@ -27,4 +27,4 @@ aws s3 cp ./ansible_jinja_tf.yml s3://${s3_install}/install/ansible/ --storage-c
 aws s3 cp ./ansible_jinja_byhost_tf.yml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
 aws s3 cp ./splunk_ansible_inventory_create.yml s3://${s3_install}/install/ansible/ --storage-class STANDARD_IA
 # j2
-aws s3 cp ./j2/splunk_ansible_inventory_template.j2 s3://${s3_install}/install/ansible/j2 --storage-class STANDARD_IA
+aws s3 cp ./j2/splunk_ansible_inventory_template.j2 s3://${s3_install}/install/ansible/j2/ --storage-class STANDARD_IA

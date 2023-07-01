d5b66e7468657915a2fa5c1852d6bc5a74a3fa01
Matthieu Araman <maraman@splunk.com>
2023-07-01 12:06:54 +0200

    move required provider to common file

--- terraform/provider-gh.tf
+++ terraform/provider-gh.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    github = {
+#      source  = "integrations/github"
+#      version = "~> 5.0"
+#    }
+#  }
+#}
 
 # Configure the GitHub Provider
 provider "github" {

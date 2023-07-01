241bca3973b52dfe0141a6449f69c12d1bb97a87
Matthieu Araman <maraman@splunk.com>
2023-07-01 17:34:43 +0200

    move to tempaltefile

--- terraform/modules/network/iam_lambda.tf
+++ terraform/modules/network/iam_lambda.tf
@@ -1,13 +1,13 @@
 
-data "template_file" "pol-splunk-route53-updatednsrecords-forlambda" {
-  template = file("policy-aws/pol-splunk-route53-updatednsrecords-forlambda.json.tpl")
-
-  vars = {
-    zone-id         = aws_route53_zone.dnszone.id
-    profile         = var.profile
-    splunktargetenv = var.splunktargetenv
-  }
-}
+#data "template_file" "pol-splunk-route53-updatednsrecords-forlambda" {
+#  template = file("policy-aws/pol-splunk-route53-updatednsrecords-forlambda.json.tpl")
+#
+#  vars = {
+#    zone-id         = aws_route53_zone.dnszone.id
+#    profile         = var.profile
+#    splunktargetenv = var.splunktargetenv
+#  }
+#}
 
 resource "aws_iam_policy" "pol-splunk-route53-updatednsrecords-forlambda" {
   name_prefix = "splunkconf_route53_updatednsrecords_forlambda"
@@ -17,24 +17,33 @@ resource "aws_iam_policy" "pol-splunk-route53-updatednsrecords-forlambda" {
   #}
   description = "Allow to update dns records from lambda at instance creation"
   #provider    = aws.region-primary
-  policy      = data.template_file.pol-splunk-route53-updatednsrecords-forlambda.rendered
+  policy      = templatefile(
+"policy-aws/pol-splunk-route53-updatednsrecords-forlambda.json.tpl",
+{
+    zone-id         = aws_route53_zone.dnszone.id
+    profile         = var.profile
+    splunktargetenv = var.splunktargetenv
+  }
+)
 }
 
-data "template_file" "pol-splunk-lambda-asg" {
-  template = file("policy-aws/pol-splunk-asg.json.tpl")
-}
+#data "template_file" "pol-splunk-lambda-asg" {
+#  template = file("policy-aws/pol-splunk-asg.json.tpl")
+#}
 
 resource "aws_iam_policy" "pol-splunk-lambda-asg" {
   description = "Permissions needed specific for ASG Lambda execution"
   #provider    = aws.region-primary
-  policy      = data.template_file.pol-splunk-lambda-asg.rendered
+  policy      = templatefile(
+"policy-aws/pol-splunk-asg.json.tpl"
+)
 }
 
 
-data "template_file" "pol-splunk-cloudwatch-write" {
-  template = file("policy-aws/cloudwatchwritepolicy.json")
-
-}
+#data "template_file" "pol-splunk-cloudwatch-write" {
+#  template = file("policy-aws/cloudwatchwritepolicy.json")
+#
+#}
 
 resource "aws_iam_policy" "pol-splunk-cloudwatch-write" {
   # ... other configuration ...
@@ -43,7 +52,9 @@ resource "aws_iam_policy" "pol-splunk-cloudwatch-write" {
   #}
   description = "Permissions needed for writing logs "
   #provider    = aws.region-primary
-  policy      = data.template_file.pol-splunk-cloudwatch-write.rendered
+  policy      = templatefile(
+"policy-aws/cloudwatchwritepolicy.json"
+)
 }
 
 

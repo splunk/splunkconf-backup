c1248e8959dcb0471a3df5dfdaeacd3379daef81
Matthieu Araman <maraman@splunk.com>
2023-07-01 18:00:07 +0200

    add default values

--- terraform/variable-gh.tf
+++ terraform/variable-gh.tf
@@ -8,30 +8,36 @@ variable enableprovision {
 variable ghtoken {
   description = "github pat token allowed to create and manage repo"
   type = string
+  default="notset"
 }
 
 variable ghowner {
   description = "github owner (where to create repo)"
   type = string
+  default="notset"
 }
 
 variable ghrepo {
   description = "github repo name"
   type = string
+  default="notset"
 }
 
 variable ghclonepat {
   description="pat token given to you for cloning"
   type=string
+  default="notset"
 }
 
 variable ghcloneuser {
   description="gh user for cloned repo"
   type=string
+  default="notset"
 }
 
 variable ghclonerepo {
   description="cloned repo name"
   type=string
+  default="notset"
 }
 

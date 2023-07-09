7ffcac98fc2fb769364774a3d5f38149306cbc7c
Matthieu Araman <maraman@splunk.com>
2023-07-09 23:57:27 +0200

    more runner support

--- terraform/iam_worker.tf
+++ terraform/iam_worker.tf
@@ -17,18 +17,18 @@ resource "aws_iam_policy" "pol-splunk-ec2worker-secret" {
   provider    = aws.region-primary
   policy      = templatefile(
 "policy-aws/pol-splunk-ec2worker-secret.json.tpl",
-{
+ {
    secret          = aws_secretsmanager_secret.splunk_admin.arn
     secret2         = module.ssh.splunk_ssh_key_arn
     ssmkey          = module.ssh.splunk_ssh_key_ssm_arn
     enableprovision = var.enableprovision
-    #ssmkeyrunner    = ( var.enableprovision ? aws_ssm_parameter.splunkpatjinjarunner[0].arn : "" )
-    ssmkeyrunner    = aws_ssm_parameter.splunkpatjinjarunner.arn
+    ssmkeyrunner    = ( var.enableprovision ? aws_ssm_parameter.splunkpatjinjarunner[0].arn : "notset" )
+    #ssmkeyrunner    = aws_ssm_parameter.splunkpatjinjarunner.arn
     profile         = var.profile
     splunktargetenv = var.splunktargetenv
-}
+ }
 
-)
+  )
 }
 
 

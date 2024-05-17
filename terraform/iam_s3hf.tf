014dc14ecabeba7324cb61e9f4e0b59a8a985918
Matthieu Araman <maraman@splunk.com>
2024-05-17 12:13:55 +0200

    comment out dynamic policy until figure out why AWS error it out

--- terraform/iam_s3hf.tf
+++ terraform/iam_s3hf.tf
@@ -1,17 +1,20 @@
+
+# Commented as this currently error out with invalid policy from AWS when not set
+
 resource "aws_iam_policy" "pol-splunk-s3-hf" {
   name_prefix = "splunkconf_s3_hf_"
   description = "Permissions needed for Splunk to access ${var.s3_bucket_1}/{var.s3_prefix_1}"
   provider    = aws.region-primary
-  policy      = templatefile(
-"policy-aws/pol-splunk-access-s3.json.tpl",
-{
-    s3_bucket       = var.s3_bucket_1
-    s3_prefix     = var.s3_prefix_1
-    profile         = var.profile
-    splunktargetenv = var.splunktargetenv
-  }
-)
-#policy = "{ \"Version\": \"2012-10-17\", \"Statement\": [ ] } "
+#  policy      = templatefile(
+#"policy-aws/pol-splunk-access-s3.json.tpl",
+#{
+#    s3_bucket       = var.s3_bucket_1
+#    s3_prefix     = var.s3_prefix_1
+#    profile         = var.profile
+#    splunktargetenv = var.splunktargetenv
+#  }
+#)
+ policy = "{ \"Version\": \"2012-10-17\", \"Statement\": [ ] } "
 
 }
 

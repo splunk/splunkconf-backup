bd1f413766b101d14376ed7a17e1e1373c8f2b46
Matthieu Araman <maraman@splunk.com>
2024-05-27 18:48:38 +0200

    update dns prefix logic

--- terraform/local-preparation-dns.tf
+++ terraform/local-preparation-dns.tf
@@ -2,7 +2,7 @@
 locals {
   # we have to create as local to be able to use a variable
   # comment and use the second version if you prefer specify it
-  dns-prefix = local.dns-prefix2 == "region-" ? format("%s-", var.region-primary) : var.dns-prefix2
+  dns-prefix = local.dns-prefix2 == "region-" ? format("%s-", var.region-primary) : local.dns-prefix2
   dns-prefix2 = var.dns-prefix == "disabled" ? "" : var.dns-prefix
   #dns-prefix=var.dns-prefix
 }

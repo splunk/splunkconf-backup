096c126b0dd01a73cc53a1c7b16c5ae1fb8b3f60
Matthieu Araman <maraman@splunk.com>
2025-08-07 14:07:28 +0200

    up to 10.0.0

--- src/splunkconf-cloud-recovery.sh
+++ src/splunkconf-cloud-recovery.sh
@@ -273,8 +273,9 @@ exec >> /var/log/splunkconf-cloud-recovery-debug.log 2>&1
 # 20250609 adding compat layer for splunkenableunifiedpartition
 # 20250610 change mke2fs options so it create more inodes per G , to prevent from a inode shortage especially with small FS and unified partition mode
 # 20250715 update/fix timer service settings
+# 20250807 up to 10.0.0
 
-VERSION="20250715a"
+VERSION="20250807a"
 
 # dont break script on error as we rely on tests for this
 set +e
@@ -1599,8 +1600,8 @@ fi # if not upgrade
 echo "#************************************** SPLUNK SOFTWARE BINARY INSTALLATION ************************"
 # Splunk installation
 # note : if you update here, that could update Splunk version at reinstanciation (redeploy backup while upgrading to this version), make sure you know what you do !
-splversion="9.4.2"
-splhash="e9664af3d956"
+splversion="10.0.0"
+splhash="e8eb0c4654f8"
 splversionhash=${splversion}-${splhash}""
 # this is spl arch, arch will be the one from os
 splarch="x86_64"

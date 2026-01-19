25449523048fae9441f0b5aa644964a2d9c08fcf
Matthieu Araman <maraman@splunk.com>
2026-01-19 20:32:40 +0100

    turn on imdsv2 required by default

--- terraform/variables-aws.tf
+++ terraform/variables-aws.tf
@@ -1,9 +1,9 @@
 # variables specific to AWS
 
 variable "imdsv2" {
-  description = "for 8.2.2+ and 8.1.5+, after configuring [imds] in server.conf to v2, you may require tokens in AWS to reduce attack surface (default=optional, set to value=required to enforce v2)"
+  description = "for 8.2.2+ and 8.1.5+, after configuring [imds] in server.conf to v2, you may require tokens in AWS to reduce attack surface (default=required which is more secure, set to value=disable to allow v1)"
   type        = string
-  default     = "optional"
+  default     = "required"
 }
 
 variable "extra_default_tags" {

f8901a53334e9510a9f65026e727e83a2a687ddf
Matthieu Araman <maraman@splunk.com>
2026-04-21 09:07:56 +0200

    add comment for semgrep

--- terraform/modules/kms/kms.tf
+++ terraform/modules/kms/kms.tf
@@ -1,7 +1,7 @@
 resource "aws_kms_key" "splunkkms" {
   #provider                = aws.region-primary
   description             = "Splunk KMS key"
-  # true by default
+  # key rotation true by default
   enable_key_rotation   = var.enable_key_rotation # semgrep exclude
   deletion_window_in_days = var.deletion_window_in_days
   rotation_period_in_days = var.rotation_period_in_days
